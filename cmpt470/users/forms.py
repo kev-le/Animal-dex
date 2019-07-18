5d4ec18ad0b0a3a6898ed5557e85cf6813fbd7db
Jon Antepyan <jantepya@sfu.ca>
2019-07-18 13:19:14 -0700

    custom users

---
+++ cmpt470/users/forms.py
@@ -0,0 +1,22 @@
+# users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm,PasswordResetForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('username', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = ('username', 'email')
+
+class CustomPasswordResetForm(PasswordResetForm):
+
+    class Meta(PasswordResetForm):
+        model = CustomUser
+        fields = ('username', 'email')
