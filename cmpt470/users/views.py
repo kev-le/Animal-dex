35a3b9d56e36ddd95d4e3c6a4555a4636db959fd
kev-le <kevin_le@sfu.ca>
2019-07-25 15:19:55 -0700

    add auto login after signup

--- cmpt470/users/views.py
+++ cmpt470/users/views.py
@@ -1,10 +1,23 @@
 # users/views.py
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('index')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        form.save()
+
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+
+        return HttpResponseRedirect(reverse_lazy('index'))
