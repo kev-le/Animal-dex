d326b3999a4a5cabad364387c75117548c5cc830
Tim Robertson <tim_robertson@sfu.ca>
2019-07-25 15:01:29 -0700

    add simple animal search
    Search form uses GET request, not POST because no side effects.
    Might move search form in future, maybe in the nav bar.

--- cmpt470/animals/urls.py
+++ cmpt470/animals/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search', views.search, name='search'),
+    path('cat/', views.cats, name='cats'),
+    path('dog/', views.dogs, name='dogs'),
+    path('bird/', views.birds, name='birds'),
+    path('cat/<slug:slug>', views.cat_detail, name='cat_detail'),
+    path('dog/<slug:slug>', views.dog_detail, name='dog_detail'),
+    path('bird/<slug:slug>', views.bird_detail, name='bird_detail')
 ]
--- data/code/add_animals_to_db.py
+++ data/code/add_animals_to_db.py
@@ -10,23 +10,85 @@
 
 import csv
 
-from animals.models import Cat, Dog
+from animals.models import Cat, Dog, Bird
 
-with open('../data/code/cats.csv') as f:
+def get_row(row):
+    row = row.strip()
+    if len(row) > 0:
+        return row
+    return None
+
+# Cats
+with open('../data/code/cat_update.csv') as f:
     reader = csv.DictReader(f, delimiter=',')
     for row in reader:
+        breed = get_row(row['breed'])
+        summary = get_row(row['summary'])
+        image = get_row(row['image'])
+        other_names = get_row(row['other_names'])
+        nicknames = get_row(row['nicknames'])
+        origin = get_row(row['origin'])
         _, created = Cat.objects.get_or_create(
-            name=row['name'],
-            image_url=row['image'],
-            summary='This is an animal'
+            name=breed,
+            image_url=image,
+            summary=summary,
+            other_names=other_names,
+            nicknames=nicknames,
+            origin=origin
+        )
+    
+# Dogs
+# (Paste after above)
+with open('../data/code/dog_update.csv') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    for row in reader:
+        breed = get_row(row['breed'])
+        summary = get_row(row['summary'])
+        image = get_row(row['image'])
+        other_names = get_row(row['other_names'])
+        nicknames = get_row(row['nicknames'])
+        origin = get_row(row['origin'])
+        weight = get_row(row['weight'])
+        height = get_row(row['height'])
+        coat = get_row(row['coat'])
+        color = get_row(row['color'])
+        lifespan = get_row(row['lifespan'])
+        _, created = Dog.objects.get_or_create(
+            name=breed,
+            image_url=image,
+            summary=summary,
+            other_names=other_names,
+            nicknames=nicknames,
+            origin=origin,
+            weight=weight,
+            height=height,
+            coat=coat,
+            color=color,
+            lifespan=lifespan
         )
 
-# This is the same as Cats for now, but will be different later
-with open('../data/code/dogs.csv') as f:
+# Birds
+# (Paste after above)
+with open('../data/code/bird.csv') as f:
     reader = csv.DictReader(f, delimiter=',')
     for row in reader:
-        _, created = Dog.objects.get_or_create(
-            name=row['name'],
-            image_url=row['image'],
-            summary='This is an animal'
+        breed = get_row(row['breed'])
+        summary = get_row(row['summary'])
+        image = get_row(row['image'])
+        conservation_status = get_row(row['conservation_status'])
+        kingdom = get_row(row['kingdom'])
+        phylum = get_row(row['phylum'])
+        order = get_row(row['order'])
+        family = get_row(row['family'])
+        binomial_name = get_row(row['binomial_name'])
+        _, created = Bird.objects.get_or_create(
+            name=breed,
+            image_url=image,
+            summary=summary,
+            conservation_status=conservation_status,
+            kingdom=kingdom,
+            phylum=phylum,
+            order=order,
+            family=family,
+            binomial_name=binomial_name
         )
