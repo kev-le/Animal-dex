6f4afdb9648619666027fbc2f61bde941832fa14
Kevin Le <kevin_le@sfu.ca>
2020-02-26 17:48:25 -0800

    setup for heroku

--- cmpt470/cmpt470/settings.py
+++ cmpt470/cmpt470/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'cmpt470.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'project',
-#         'USER': 'project',
-#         'PASSWORD': 'secret',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'project',
+        'USER': 'project',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'project',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -150,3 +150,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import django_heroku
+django_heroku.settings(locals())
